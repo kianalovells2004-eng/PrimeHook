d648c22e26cbe05ac67063d1a8c3c5430a02c46f
kianalovells2004-eng <kianalovells2004@gmail.com>
2026-08-31 17:41:52 -0700

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,4 +1,3 @@
--- Load PepsiUI
 local Library = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/Stratxgy/PepsiUI/refs/heads/main/pepsi.lua"
 ))()
@@ -7,8 +6,7 @@ _G.GunModToggles = {
     FastFire = false,
     InfRange = false,
     NoSpread = false,
-    Tracer = false,       -- keep this if you still want your own beam
-    RgbTracer = false,    -- NEW: enables RGB game tracers
+    -- Tracer and RgbTracer removed
 }
 
 local Window = Library:CreateWindow({ Name = "PrimeHook" })
@@ -39,24 +37,5 @@ Section:AddToggle({
     Callback = function()
         _G.GunModToggles.NoSpread = not _G.GunModToggles.NoSpread
         print("NoSpread:", _G.GunModToggles.NoSpread)
-    end
-})
-
-Section:AddToggle({
-    Name = "Bullet Tracer",   -- your custom beam (keep if you want)
-    Side = "Left",
-    Callback = function()
-        _G.GunModToggles.Tracer = not _G.GunModToggles.Tracer
-        print("Tracer:", _G.GunModToggles.Tracer)
-    end
-})
-
--- NEW: RGB Tracer toggle
-Section:AddToggle({
-    Name = "RGB Tracers",
-    Side = "Left",
-    Callback = function()
-        _G.GunModToggles.RgbTracer = not _G.GunModToggles.RgbTracer
-        print("RGB Tracers:", _G.GunModToggles.RgbTracer)
     end
 })
